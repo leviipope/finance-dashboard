a15cdff8e5e6f47ea6a54a94b293cb667d442446
leviipope <150926233+leviipope@users.noreply.github.com>
2025-06-10 15:02:40 +0200

    feat: load, edit, save main dataframe, and merge it with new bank statements

--- app.py
+++ app.py
@@ -1,34 +1,67 @@
 import pandas as pd
 import streamlit as st
+import os
 
 st.set_page_config(page_title="Simple finance app", page_icon=":money_with_wings:", layout="wide") 
+
+MAIN_DATAFRAME_FILE = "main_dataframe.csv"
 
 def load_statement(file):
     try: 
         df = pd.read_csv(file)
         df = df.drop(["Fee","Completed Date","Currency","State"], axis=1)
         df = df[df["Type"] != "INTEREST"]
-        # Finished coding here
-        # Tried to check the type of the data in the amounts column
+        df['Started Date'] = pd.to_datetime(df['Started Date'])
+        df = df.rename(columns={"Started Date": "Date"})
+        df["Hide"] = False
 
         return df
     except Exception as e:
         st.error(f"Error reading the file: {str(e)}")
         return None
 
+def load_main_dataframe():
+    try:
+        df = pd.read_csv(MAIN_DATAFRAME_FILE)
+        return df
+    except FileNotFoundError:
+        st.write("Could not find main_dataframe.csv")
+        return None
+
+def merge_dataframes(main_df, new_df):
+    combined_df = pd.concat([main_df, new_df]).drop_duplicates(subset=['Date', 'Description', 'Amount'], keep='last')
+    return combined_df
+
+def save_main_dataframe(df):
+    df.to_csv(MAIN_DATAFRAME_FILE, index=False)
+
 
 def main():
-    page = st.sidebar.radio("Go to", ["Edit Dataframe", "Analytics etc."])
+    page = st.sidebar.radio("Go to", ["Customize Data", "Spending Analytics", "Incoming Analytics"])
 
-    if page == "Edit Dataframe":
-        st.title("Revolut Analytics Dashboard")
+    if page == "Customize Data":
+        st.title("Main DataFrame")
+        main_df = load_main_dataframe()
+        if main_df is not None:
+            column_config = {col: st.column_config.Column(col, disabled=True) for col in main_df.columns if col != 'Hide'}
+            column_config['Hide'] = st.column_config.CheckboxColumn('Hide')
 
-        # upload_file = st.file_uploader("Upload your Revolut statement", type=["csv"])
-        upload_file = "./account-statement.csv"
+            main_df_to_edit = st.data_editor(main_df, column_config=column_config)
 
+            if st.button("Apply Changes"):
+                save_main_dataframe(main_df_to_edit)
+        else:
+            st.error("No data available to edit")
+
+
+        upload_file = st.file_uploader("Upload your new Revolut statement", type=["csv"])
         if upload_file is not None:
-            df = load_statement(upload_file)
-            main_df_to_edit = st.data_editor(df)
+            new_df = load_statement(upload_file)
+            updated_df = merge_dataframes(main_df, new_df)
+            save_main_dataframe(updated_df)
+
+
+
 
         
 main()
